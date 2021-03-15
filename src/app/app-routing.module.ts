0057e5dba50971c486e7ca47e62feb985518936e
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2021-03-15 11:53:11 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 import { CreateAdminComponent } from './views/admins/create-admin/create-admin.component';
 import { IndexAdminComponent } from './views/admins/index-admin/index-admin.component';
 import { UpdateAdminComponent } from './views/admins/update-admin/update-admin.component';
 import { CreateDeptComponent } from './views/departments/create-dept/create-dept.component';
 import { IndexDeptComponent } from './views/departments/index-dept/index-dept.component';
 import { SearchDeptComponent } from './views/departments/search-dept/search-dept.component';
+import { ShowDeptComponent } from './views/departments/show-dept/show-dept.component';
 import { CreateEmpComponent } from './views/employees/create-emp/create-emp.component';
 import { IndexEmpComponent } from './views/employees/index-emp/index-emp.component';
-import { SearchEmpComponent } from './views/employees/search-emp/search-emp.component';
+import { ShowEmpComponent } from './views/employees/show-emp/show-emp.component';
+import { UpdateEmpComponent } from './views/employees/update-emp/update-emp.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { NotFoundComponent } from './views/shared/not-found/not-found.component';
@@ -21,6 +24,7 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuardService],
     children: [
       //dept childs
       {
@@ -31,6 +35,9 @@ const routes: Routes = [
       },
       {
         path: 'show-all-departments', component: IndexDeptComponent
+      },
+      {
+        path: 'show-department/:id', component: ShowDeptComponent
       },
       // admin routes
       {
@@ -47,10 +54,13 @@ const routes: Routes = [
         path: 'add-employee', component: CreateEmpComponent
       },
       {
-        path: 'search-employee', component: SearchEmpComponent
+        path: 'show-employee/:id', component: ShowEmpComponent
       },
       {
         path: 'show-all-employees', component: IndexEmpComponent
+      },
+      {
+        path: 'update-employee/:id', component: UpdateEmpComponent
       },
     ],
   },
